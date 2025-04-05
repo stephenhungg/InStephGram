ab4385a9f63f6a0280c20dea37b676de58cee18e
stephenhung <102489058+stephenhungg@users.noreply.github.com>
2025-04-05 00:39:16 -0700

    fixed like button

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Container, VStack, Link, Text, Box, Image, Heading, Button, HStack, Icon } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import { FaRegThumbsDown } from 'react-icons/fa';
 import { useUserGlobal } from '../global/user';
 import { apiRequest } from '../utils/api';
 
@@ -193,7 +193,7 @@ const HomePage = () => {
                                             p={2}
                                         >
                                             <Icon
-                                                as={isPostLiked(post) ? FaHeart : FaRegHeart}
+                                                as={isPostLiked(post) ? FaRegThumbsDown : FaRegThumbsDown}
                                                 boxSize="24px"
                                                 color={isPostLiked(post) ? "red.500" : "gray.400"}
                                             />
